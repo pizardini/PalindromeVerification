93df0bd8081b1ed89f3a58ce88be735b9781b057
Pietro Zardini <pizardini@gmail.com>
2023-09-22 09:37:34 -0300

    criação dos métodos base

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+
+    public List<String> input() {
+        List<String> words = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite as palavras que gostaria de verificar, uma por linha.");
+
+        String line;
+        while (!(line = sc.nextLine().trim()).isEmpty()) {
+            words.add(line);
+        }
+        return words;
+    }
+
+    public int output(List<String> words) {
+
+        for (String word : words) {
+            Palindrome.isPalindrome(word);
+        }
+        return 0;
     }
 }
---
+++ src/Palindrome.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Palindrome {
+    public static String reverseString(String str) {
+        int length = str.length();
+        StringBuilder result = new StringBuilder(length);
+
+        for (int i = length - 1; i >= 0; i--) {
+            result.append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
+    public static boolean isPalindrome(String word) {
+        String reversed = reverseString(word);
+        return reversed.equals(word);
+    }
+
+    public static boolean anagrammPalindrome(String word) {
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : word.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        int oddCount = 0;
+        for (int count : charCount.values()) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        return oddCount <= 1;
+    }
+
+    public int sumPalindromes(boolean word) {
+        int contador= 0;
+        if (word) {
+            contador++;
+        }
+        return contador;
+    }
+
+    public static boolean isPalindrome2(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
